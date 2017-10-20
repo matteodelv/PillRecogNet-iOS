d3a7ad34546870a7c9f7e8c60a150ea78d57939a
Matteo Del Vecchio <delvecchiomatteo96@gmail.com>
2017-10-20 17:34:27 +0200

    Initial Commit

---
+++ TirocinioiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TirocinioiOSApp
+//
+//  Created by Matteo Del Vecchio on 17/10/17.
+//  Copyright © 2017 Matteo Del Vecchio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
