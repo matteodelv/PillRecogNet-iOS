9296adccb500e2af79834e1d53cf36f23ca7900f
Matteo Del Vecchio <delvecchiomatteo96@gmail.com>
2017-12-06 19:12:56 +0100

    Added notification auth checks

--- PillRecogNetApp/ReminderTableViewController.swift
+++ PillRecogNetApp/ReminderTableViewController.swift
@@ -54,46 +54,57 @@ class ReminderTableViewController: UITableViewController {
 	}
 	
 	@objc func setReminder() {
-		let identifier = currentClassification.objectID.uriRepresentation().absoluteString
-		let center = UNUserNotificationCenter.current()
-		center.removeDeliveredNotifications(withIdentifiers: [identifier])
-		center.removePendingNotificationRequests(withIdentifiers: [identifier])
-		
-		var repeating = false
-		if let cell = tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? RepeatingCell {
-			repeating = cell.repeatingSwitch.isOn
-		}
-		
-		var dateComponents:DateComponents
-		if repeating {
-			dateComponents = Calendar.current.dateComponents([.hour, .minute], from: datePicker.date)
-		} else {
-			dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: datePicker.date)
-		}
-		
-		let sortDescriptor = NSSortDescriptor(key: #keyPath(Match.probability), ascending: false)
-		let sortedMatches = currentClassification.matches?.sortedArray(using: [sortDescriptor]) as! [Match]
-		
-		let content = UNMutableNotificationContent()
-		content.title = "Medicinale da Assumere!"
-		content.sound = UNNotificationSound.default()
-		content.body = "Ricordati che devi assumere \(sortedMatches[0].label ?? "un medicinale")... Provvedi il prima possibile!"
-		content.userInfo = ["ClassificationID": identifier]
-		content.categoryIdentifier = "PillNotificationCategory"
-		
-		let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: repeating)
-		let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-		
-		center.add(request, withCompletionHandler: { (error) in
-			if error != nil {
+		UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+			if settings.authorizationStatus == .notDetermined || settings.authorizationStatus == .denied {
 				DispatchQueue.main.async {
-					let alert = UIAlertController.errorAlertWith(message: "Si è verificato un errore durante l'impostazione del promemoria. Riprovare.")
+					let alert = UIAlertController.errorAlertWith(message: "L'applicazione non dispone delle necessarie autorizzazioni per impostare le notifiche. Controlla nelle Impostazioni.")
 					self.present(alert, animated: true, completion: nil)
 				}
 			} else {
-				self.dismiss(animated: true, completion: nil)
+				DispatchQueue.main.async {
+				let identifier = self.currentClassification.objectID.uriRepresentation().absoluteString
+				let center = UNUserNotificationCenter.current()
+				center.removeDeliveredNotifications(withIdentifiers: [identifier])
+				center.removePendingNotificationRequests(withIdentifiers: [identifier])
+				
+				var repeating = false
+				if let cell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? RepeatingCell {
+					repeating = cell.repeatingSwitch.isOn
+				}
+				
+				var dateComponents:DateComponents
+				if repeating {
+					dateComponents = Calendar.current.dateComponents([.hour, .minute], from: self.datePicker.date)
+				} else {
+					dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: self.datePicker.date)
+				}
+				
+				let sortDescriptor = NSSortDescriptor(key: #keyPath(Match.probability), ascending: false)
+				let sortedMatches = self.currentClassification.matches?.sortedArray(using: [sortDescriptor]) as! [Match]
+				
+				let content = UNMutableNotificationContent()
+				content.title = "Medicinale da Assumere!"
+				content.sound = UNNotificationSound.default()
+				content.body = "Ricordati che devi assumere \(sortedMatches[0].label ?? "un medicinale")... Provvedi il prima possibile!"
+				content.userInfo = ["ClassificationID": identifier]
+				content.categoryIdentifier = "PillNotificationCategory"
+				
+				let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: repeating)
+				let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+				
+				center.add(request, withCompletionHandler: { (error) in
+					if error != nil {
+						DispatchQueue.main.async {
+							let alert = UIAlertController.errorAlertWith(message: "Si è verificato un errore durante l'impostazione del promemoria. Riprovare.")
+							self.present(alert, animated: true, completion: nil)
+						}
+					} else {
+						self.dismiss(animated: true, completion: nil)
+					}
+				})
+				}
 			}
-		})
+		}
 	}
 
 }
